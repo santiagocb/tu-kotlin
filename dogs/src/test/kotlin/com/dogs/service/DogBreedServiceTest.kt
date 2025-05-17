5ac6dfec4c1ec48e6ba0c69011c286de0a3082f6
Santiago <santiagocadavidbte@gmail.com>
2025-05-16 22:31:02 -0500

    Unit tests for Service

--- dogs/src/test/kotlin/com/dogs/service/DogBreedServiceTest.kt
+++ dogs/src/test/kotlin/com/dogs/service/DogBreedServiceTest.kt
@@ -3,18 +3,15 @@ package com.dogs.service
 import com.dogs.api.DogBreedApiClient
 import com.dogs.model.DogBreed
 import com.dogs.model.DogBreedResponse
+import com.dogs.model.DogSubBreedResponse
 import com.dogs.repository.DogBreedRepository
 import io.mockk.*
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.cache.CacheManager
-import org.springframework.cache.caffeine.CaffeineCacheManager
-import org.springframework.cache.concurrent.ConcurrentMapCache
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.client.WebClientResponseException
@@ -77,6 +74,79 @@ class DogBreedServiceTest {
         coVerify(exactly = 1) { dogBreedRepository.findAll() }
     }
 
+    @Test
+    fun `getBreedsWithoutSubBreeds should only include breeds without subBreeds`() = runBlocking {
+        // Given
+        val mockData = flowOf(
+            DogBreed(id = 1, breed = "bulldog", subBreed = "english,french"),
+            DogBreed(id = 2, breed = "beagle", subBreed = null),
+            DogBreed(id = 3, breed = "chihuahua", subBreed = ""),
+        )
+        val expectedResponse = listOf(
+            DogBreedResponse(breed = "beagle", subBreeds = null),
+            DogBreedResponse(breed = "chihuahua", subBreeds = null)
+        )
+        coEvery { dogBreedRepository.findAll() } returns mockData
+
+        // When
+        val result = dogBreedService.getBreedsWithoutSubBreeds().toList()
+
+        // Then
+        assertEquals(expectedResponse, result)
+        coVerify(exactly = 1) { dogBreedRepository.findAll() }
+    }
+
+    @Test
+    fun `getSubBreedsForBreed should return subBreeds when subBreed is present`() = runBlocking {
+        // Given
+        val breed = "bulldog"
+        val mockDogBreed = DogBreed(id = 1, breed = breed, subBreed = "english,french")
+        val expectedResponse = listOf(
+            DogSubBreedResponse(subBreed = "english"),
+            DogSubBreedResponse(subBreed = "french")
+        )
+
+        coEvery { dogBreedRepository.findByBreed(breed) } returns mockDogBreed
+
+        // When
+        val result = dogBreedService.getSubBreedsForBreed(breed)
+
+        // Then
+        assertEquals(expectedResponse, result)
+        coVerify(exactly = 1) { dogBreedRepository.findByBreed(breed) }
+    }
+
+
+    @Test
+    fun `getSubBreedsForBreed should return empty list when subBreed is null`() = runBlocking {
+        // Given
+        val breed = "beagle"
+        val mockDogBreed = DogBreed(id = 2, breed = breed, subBreed = null)
+        val expectedResponse = emptyList<DogSubBreedResponse>()
+        coEvery { dogBreedRepository.findByBreed(breed) } returns mockDogBreed
+
+        // When
+        val result = dogBreedService.getSubBreedsForBreed(breed)
+
+        // Then
+        assertEquals(expectedResponse, result)
+        coVerify(exactly = 1) { dogBreedRepository.findByBreed(breed) } // Ensure repository is called
+    }
+
+    @Test
+    fun `getSubBreedsForBreed should return null when breed is not found`() = runBlocking {
+        // Given
+        val breed = "unknown"
+        val expectedResponse: List<DogSubBreedResponse>? = null
+        coEvery { dogBreedRepository.findByBreed(breed) } returns null
+
+        // When
+        val result = dogBreedService.getSubBreedsForBreed(breed)
+
+        // Then
+        assertEquals(expectedResponse, result)
+        coVerify(exactly = 1) { dogBreedRepository.findByBreed(breed) }
+    }
 
     @Test
     fun `getBreedImage should returns image from database if present`() = runBlocking {
